10352858834db9dc2664f8837d31aed3bd78e0c3
chavansanket <sanketc1297@gmail.com>
2025-01-06 09:39:30 +0530

    first commit

---
+++ spring-sec-2/src/main/java/com/project2/sec/SpringSec2Application.java
@@ -0,0 +1,13 @@
+package com.project2.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSec2Application.class, args);
+	}
+
+}
